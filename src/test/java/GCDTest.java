95c3a05aba0421c3aea4d014f717041aaf30568a
Amanda Huang <79762895+AmandaJ-Huang@users.noreply.github.com>
2021-11-30 22:30:49 -0500

    Compared timing GCD vs GCD2

--- src/test/java/GCDTest.java
+++ src/test/java/GCDTest.java
@@ -1,6 +1,9 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.time.Duration;
+import java.time.Instant;
+
 public class GCDTest {
 
     @Test
@@ -25,5 +28,23 @@ public class GCDTest {
 
         // Then
         Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGCDVSGCD2() {
+        // Given
+        Instant start = Instant.now();
+        Integer gcd = GCD.gcd(4200, 3780);
+        Instant finish = Instant.now();
+        long timeElapsed = Duration.between(start, finish).toMillis();
+
+        // When
+        Instant start2 = Instant.now();
+        Integer gcd2 = GCD.gcd(132, 84);
+        Instant finish2 = Instant.now();
+        long timeElapsed2 = Duration.between(start2, finish2).toMillis();
+
+        // Then
+        Assert.assertTrue(timeElapsed2 < timeElapsed);
     }
 }
